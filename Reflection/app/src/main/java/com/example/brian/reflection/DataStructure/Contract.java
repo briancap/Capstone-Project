2a8adfdee86c93635f92d8c03fb264d9655716b0
briancap <btc5081@gmail.com>
2017-04-05 15:03:34 -0400

    add layout files also started on the DataStructe Contract

--- Reflection/app/src/main/java/com/example/brian/reflection/DataStructure/Contract.java
+++ Reflection/app/src/main/java/com/example/brian/reflection/DataStructure/Contract.java
@@ -1,8 +1,30 @@
 package com.example.brian.reflection.DataStructure;
 
-/**
- * Created by Brian on 4/4/2017.
- */
+import android.net.Uri;
+import android.provider.BaseColumns;
+
 
 public class Contract {
+
+    public static final String AUTHORITY = "com.example.brian.reflection";
+    public static final Uri BASE_URI = Uri.parse("content://" + AUTHORITY);
+
+    public static final String refltionTableName    = "reflection";
+    public static final String promptTableName      = "prompt";
+
+
+    public static final class ReflectionTable implements BaseColumns{
+
+        public static final String COLUMN_ID_AI = "_id";
+        public static final String
+
+
+    }
+
+
+
+
+
+
+
 }
