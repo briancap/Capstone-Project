e5afaa66aba0ac69a179db33517169b27f81874b
briancap <btc5081@gmail.com>
2017-05-13 18:01:33 -0400

    layout and menu work - dependencies to gradle - Prompts activity to manifest - container to MainActivity - fab add - RecyclerView and FAB to fragment_reflection_list - menu item and intent to Prompts+Settings - create dimens.xml file - create menu.xml file

--- Reflection/app/src/main/java/com/example/brian/reflection/MainActivity.java
+++ Reflection/app/src/main/java/com/example/brian/reflection/MainActivity.java
@@ -1,10 +1,13 @@
  package com.example.brian.reflection;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +16,23 @@ public class MainActivity extends AppCompatActivity {
 
         FragmentManager fm = getSupportFragmentManager();
         fm.beginTransaction().add(R.id.container, new ReflectionListFragment()).commit();
-
     }
-}
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        switch(menuItem.getItemId()) {
+            case R.id.prompts_and_settings:
+                Intent intent = new Intent(getApplicationContext(), PromptsAndSettingsActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+}//EOC
--- Reflection/app/src/main/java/com/example/brian/reflection/PromptsAndSettingsActivity.java
+++ Reflection/app/src/main/java/com/example/brian/reflection/PromptsAndSettingsActivity.java
@@ -3,12 +3,14 @@ package com.example.brian.reflection;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.BindView;
+
 public class PromptsAndSettingsActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_prompts_and_settings);
     }
 
 }
