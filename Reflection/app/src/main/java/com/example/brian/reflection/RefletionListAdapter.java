8533fe49cf672adb4ffdc15a775d37f9e4935bd3
briancap <btc5081@gmail.com>
2017-05-17 11:11:23 -0400

    adapter work 2

--- Reflection/app/src/main/java/com/example/brian/reflection/RefletionListAdapter.java
+++ Reflection/app/src/main/java/com/example/brian/reflection/RefletionListAdapter.java
@@ -3,11 +3,7 @@ package com.example.brian.reflection;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 
-/**
- * Created by Brian on 4/4/2017.
- */
 
 //TODO: neds to extend RecyclerView.Adapter
 public class RefletionListAdapter extends RecyclerView.Adapter<RefletionListAdapter.ReflectionViewHolder> {
@@ -22,7 +18,7 @@ public class RefletionListAdapter extends RecyclerView.Adapter<RefletionListAdap
     public void onBindViewHolder(ReflectionViewHolder holder, int position) {
 
     }
-    
+
     @Override
     public int getItemCount() {
         return 0;
